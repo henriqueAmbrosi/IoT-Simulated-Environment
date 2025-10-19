52763ceaced370f285485f0ef053d8bf493ee011
henriqueAmbrosi <henriqueambrosi1256@gmail.com>
2025-10-19 20:32:56 -0300

    Fixing topic creation issue

--- init.sh
+++ init.sh
@@ -8,21 +8,37 @@ echo "Iniciando o ambiente Docker Compose..."
 echo "==========================================="
 docker compose up -d
 
+# -----------------------------
 # Espera o ThingsBoard iniciar
+# -----------------------------
 echo "Aguardando ThingsBoard iniciar..."
 until [ "$(docker inspect -f '{{.State.Running}}' $TB_CONTAINER 2>/dev/null)" == "true" ]; do
     sleep 5
     echo "ThingsBoard ainda não está pronto..."
 done
 
-echo "ThingsBoard está rodando."
+# -----------------------------
+# Popula o banco de dados se necessário
+# -----------------------------
 
-echo "Verificando necessidade de popular DB do ThingsBoard"
-if docker compose run --rm -e INSTALL_TB=true -e LOAD_DEMO=true $TB_CONTAINER >/dev/null 2>&1; then
-    echo "DB populado com sucesso!"
-else
-    echo "População já foi realizada. Continuando..."
-fi
+ATTEMPT=1
+MAX_ATTEMPTS=10
+
+until docker exec kafka kafka-topics.sh --bootstrap-server kafka:9092 --list 2>/dev/null | grep -q "tb_rule_engine.hp.0"; do
+    echo "Executando inicialização do ThingsBoard (Tentativa $ATTEMPT/$MAX_ATTEMPTS)"
+    docker compose run --rm -e INSTALL_TB=true -e LOAD_DEMO=true "$TB_CONTAINER" >/dev/null 2>&1 || true 
+    docker compose restart "$TB_CONTAINER" >/dev/null 2>&1
+
+    sleep 10
+    ATTEMPT=$((ATTEMPT + 1))
+
+    if [ $ATTEMPT -gt $MAX_ATTEMPTS ]; then
+        echo "Não foi possível inicializar o ThingsBoard após $MAX_ATTEMPTS tentativas."
+        exit 1
+    fi
+
+    
+done
 
 echo "==========================================="
 echo "Ambiente IoT pronto para uso!"
